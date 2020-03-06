880e25c8aed4573ed1267422b9f41ffe1f35c6d9
Acelancer9099 <61864136+Acelancer9099@users.noreply.github.com>
2020-03-06 13:09:18 +0530

    test

---
+++ sample portfolio.php
@@ -0,0 +1,227 @@
+
+<html >
+<head>
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title> Portfolio Page</title>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<nav class="navbar navbar-default">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myDefaultNavbar1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand" href="#"><strong>ANANT RATHI</strong></a> </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="myDefaultNavbar1">
+      <ul class="nav navbar-nav">
+      <li ><a href="/learning/contact.php.php"><strong>	Contact </strong><span class="sr-only">(current)</span></a></li>
+        <li ><a  href=".."> <strong>Resume </strong></a></li>
+        <li class="dropdown" > <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><strong>projects</strong> <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Web Development</a></li>
+            <li><a href="#">Software Developent</a></li>
+            <li><a href="#">Game Development</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Freelance</a></li>
+            <li class="divider"></li>
+            <li><a href="#">On Leisure Front</a></li>
+          </ul>
+        </li>
+      </ul>
+    <!--  <form class="navbar-form navbar-right" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>--> 
+</div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid --> 
+</nav>
+<section>
+  <div class="jumbotron text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs12">
+          <h1>Budding Software Developer!</h1>
+          <p> Anant Rathi has done diverse projects including iOS/Android apps, Mac/Windows programs and web apps for clients. He has done work in software development, mobile app creation, front-end/back-end, database/server management, graphic design, and video game development. Anant loves programming, speaking, traveling, and lifting heavy things. What he does not love is talking about himself in the 3<sup>rd</sup> person. For more info, check out his  projects.</p>
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Explore More »</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section><!--
+ <div class="col-sm-4 col-lg-5">
+        <h2>Skill Set</h2>
+       <hr>
+         Green Progress Bar 
+        <div class="progress">
+          <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 85%"> HTML</div>
+        </div>
+         Blue Progress Bar 
+        <div class="progress">
+          <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 80%"> CSS</div>
+        </div>
+         Yellow Progress Bar 
+        <div class="progress">
+          <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%"> JAVASCRIPT</div>
+        </div>
+         Red Progress Bar 
+        <div class="progress">
+          <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 60%"> PHP</div>
+        </div>
+        <div class="progress">
+          <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 55%"> WORDPRESS</div>
+        </div>
+        <div class="progress">
+          <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%"> PHOTOSHOP</div>
+        </div>
+        <div class="progress">
+          <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%"> ILLUSTRATOR</div>
+        </div>-->
+<!--<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 text-center">
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, magni, doloribus, possimus eum sapiente deleniti doloremque fugit ut expedita molestiae iusto debitis eveniet modi obcaecati ipsam quos quis labore dicta.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<section>
+  <div class="container well">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-6">
+        <div class="media">
+          <div class="media-left"> <a href="#"> <img class="media-object" src="img/35X35.gif" alt="..."> </a> </div>
+          <div class="media-body">
+            <h2 class="media-heading">Media heading</h2>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-6">
+        <div class="media">
+          <div class="media-left"> <a href="#"> <img class="media-object" src="img/35X35.gif" alt="..."> </a> </div>
+          <div class="media-body">
+            <h2 class="media-heading">Media heading</h2>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-6">
+        <div class="media">
+          <div class="media-left"> <a href="#"> <img class="media-object" src="img/35X35.gif" alt="..."></a></div>
+          <div class="media-body">
+            <h2 class="media-heading">Media heading</h2>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section>
+	<div class="container">
+		<div class="row">
+        	<div class="col-lg-12 text-center">
+            	<h2>If brute force doesn’t solve your problems, then you aren’t using enough.</h2>
+            	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, et, placeat !</p>
+            </div>
+        </div>
+        <div class="row">
+        	<div class="col-md-6 text-center">
+            	<img src="img/400x400.gif" alt="">
+                <h3>Lorem ipsum dolor sit amet, consectetur elit</h3>
+                <h6>Lorem ipsum dolor sit amet, consectetur elit</h6>
+            </div>
+        	<div class="col-md-6 text-center">
+            	<img src="img/400x400.gif" alt="">
+                <h3>Lorem ipsum dolor sit amet, consectetur elit</h3>
+                <h6>Lorem ipsum dolor sit amet, consectetur elit</h6>
+            </div>
+        </div>
+        <div class="row">
+        	<div class="col-md-3 text-center col-sm-6 col-xs-6"><img src="img/200x200.gif" alt=""></div>
+        	<div class="col-md-3 text-center col-sm-6 col-xs-6"><img src="img/200x200.gif" alt=""></div>
+			<div class="col-md-3 text-center col-sm-6 col-xs-6 hidden-xs hidden-sm"><img src="img/200x200.gif" alt=""></div>
+        	<div class="col-md-3 text-center col-sm-6 col-xs-6 hidden-xs hidden-sm"><img src="img/200x200.gif" alt=""></div>
+      </div>
+      <hr>
+        <div class="row">
+          <div class="col-md-3 text-center col-sm-6 col-xs-6"><img src="img/200x200.gif" alt=""></div>
+          <div class="col-md-3 text-center col-sm-6 col-xs-6"><img src="img/200x200.gif" alt=""></div>
+          <div class="col-md-3 text-center col-sm-6 col-xs-6 hidden-xs hidden-sm"><img src="img/200x200.gif" alt=""></div>
+          <div class="col-md-3 text-center col-sm-6 col-xs-6 hidden-xs hidden-sm"><img src="img/200x200.gif" alt=""></div>
+        </div>
+	</div>
+</section>
+--><hr>
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1>Ruby is rubbish! PHP is phpantastic!</h1>
+     	  <h3> If you can change my mind , then <h3>
+		<a href="/learning/contact.php.php" class="btn btn-info" role="button">Get in touch</a>
+        <!--<button type="button" class="btn btn-dark"  href="/learning/contact.php.php">Get in touch.</button>
+-->      </div>
+    </div>
+  </div>
+</section>
+<hr>
+<!--<div class="section well">
+    <div class="container">
+   	  <div class="row">
+		<div class="col-lg-4 col-md-4">
+            <h3 class="text-center">WHO WE ARE</h3>
+            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, ducimus, sit, quibusdam quidem recusandae veniam eos quod error nisi repellat excepturi laboriosam aspernatur suscipit possimus consectetur dolores nihil labore quas eius illo accusamus nulla sed blanditiis porro accusantium. Perspiciatis, perferendis!</h5>
+        </div>
+		<div class="col-lg-4 col-md-4">
+		  <h3 class="text-center">GET IN TOUCH</h3>
+          <address class="text-center">
+  <strong>MyCompany, Inc.</strong><br>
+  Sunny Autumn Plaza, Grand Coulee,<br>
+  CA, 91308-4075, US<br>
+  <abbr title="Phone">P:</abbr> (123) 456-7890
+</address>
+</div>
+		<div class="col-lg-4 col-md-4">
+		  <h3 class="text-center">NEWSLETTER</h3>
+          <form>
+    <div class="form-group col-lg-9 col-md-8 col-sm-10 col-xs-10">
+<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+    </div>
+<button type="submit" class="btn btn-default">Subscribe<br>
+</button>
+</form>
+		</div>
+</div>
+    </div>
+	</div>-->
+    <footer class="text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <p></p>
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="js/jquery-1.11.2.min.js"></script> 
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
